b1f991b5374581ccd3e04685e920e4c4111216e5
ahmedawan216 <ahmediqbalawan124@gmail.com>
2025-12-28 00:03:25 +0500

    02-folder-props

---
+++ 02-folder-components/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Malik Ahmed Awan</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Iure maiores odit placeat, esse rerum voluptatem.
+      </div>
+    </div>
+  )
+}
+
+export default card
