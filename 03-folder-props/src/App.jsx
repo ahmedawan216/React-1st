69fecba1b63f4629b05d4d8d5d469096fbf9cb16
ahmedawan216 <ahmediqbalawan124@gmail.com>
2025-12-29 00:03:01 +0500

    03-folder-props: Added Card component

--- 03-folder-props/src/App.jsx
+++ 03-folder-props/src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Card from "./components/card";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='parent'>
+      <Card user="Ahmed Awan" age={20} img='https://images.unsplash.com/photo-1764336312138-14a5368a6cd3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDc1fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D'/>
+      <Card user="King" age={19} img='https://images.unsplash.com/photo-1765498068996-1e4c0bfd2219?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D'/>
+      <Card user="Rex" age={23} img='https://images.unsplash.com/photo-1763411711221-40166c5e20cd?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDg4fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D'/>
+    </div>
   )
 }
 
